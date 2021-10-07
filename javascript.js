1b60f66640f5e0747da53e5374ec7d6dc74c311f
Sufi Gaffar <sufi@designmynight.com>
2021-10-07 12:12:14 +0100

    chore: ensure switch statements are indented with 4 spaces

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ module.exports = {
     "airbnb"
   ],
   rules: {
-    // 4 space indentation
-    "indent": ["error", 4],
+    // 4 space indentation (including in switch statements)
+    "indent": ["error", 4, {"SwitchCase": 1}],
     "react/jsx-indent": ["error", 4],
     'react/jsx-indent-props': ['error', 4],
     // Allow ++ operator
