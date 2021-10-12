0b9e015749cdbd5f0b0df53355cb8f2b2f56f92e
Sufi Gaffar <sufi@designmynight.com>
2021-10-12 10:12:55 +0100

    chore: only apply TS rules to TS files

--- typescript.js
+++ typescript.js
@@ -1,34 +1,42 @@
 module.exports = {
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
     "./javascript"
   ],
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    // 4 space indentation
-    "@typescript-eslint/indent": ["error", 4],
-    // Allows Typescript files to have tsx.
-    "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".ts"] }],
-    // Allows omission of JS/TS extensions.
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-    ]
-  },
-  settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+  overrides: [
+    {
+      extends: [
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "./javascript"
+      ],
+      plugins: [
+        '@typescript-eslint',
+      ],
+      files: ['**/*.tsx', '**/*.ts'],
+      rules: {
+        // 4 space indentation
+        "@typescript-eslint/indent": ["error", 4],
+        // Allows Typescript files to have tsx.
+        "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".ts"] }],
+        // Allows omission of JS/TS extensions.
+        "import/extensions": [
+          "error",
+          "ignorePackages",
+          {
+            "js": "never",
+            "jsx": "never",
+            "ts": "never",
+            "tsx": "never"
+          }
+        ]
+      },
+      settings: {
+        "import/resolver": {
+          "node": {
+            "extensions": [".js", ".jsx", ".ts", ".tsx"]
+          }
+        }
       }
     }
-  }
+  ]
 }
